c87cc2f4577e97a56d5103b9c9bf7e275bfc614c
Johnny Cai <zhongjie.cai@sap.com>
2019-09-23 13:23:40 +0200

    Remove capturing groups

--- server/model/parameter.go
+++ server/model/parameter.go
@@ -10,10 +10,10 @@ const (
 	ParameterTypeInteger  ParameterType = `^\d+$`
 	ParameterTypeUUID     ParameterType = `^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`
 	ParameterTypeDate     ParameterType = `^[\d]{4}-[\d]{2}-[\d]{2}$`
-	ParameterTypeTime     ParameterType = `^[\d]{2}:[\d]{2}:[\d]{2}(\.[\d]{3})?$`
-	ParameterTypeDateTime ParameterType = `^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}(\.[\d]{3})?Z([\d]{2}:[\d]{2})?$`
-	ParameterTypeBoolean  ParameterType = `^(?i)(true|false)$`
-	ParameterTypeFloat    ParameterType = `^\d+(\.\d+)?$`
+	ParameterTypeTime     ParameterType = `^[\d]{2}:[\d]{2}:[\d]{2}(?:\.[\d]{3})?$`
+	ParameterTypeDateTime ParameterType = `^[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}(?:\.[\d]{3})?Z(?:[\d]{2}:[\d]{2})?$`
+	ParameterTypeBoolean  ParameterType = `^(?i)(?:true|false)$`
+	ParameterTypeFloat    ParameterType = `^\d+(?:\.\d+)?$`
 )
 
 // Evaludate evaluates the given value against the parameter type's defined regex expression and returns whether or not the regex matches
